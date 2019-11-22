d8de0c2c8bfffbe96a5c8f94f66d0442034f928f
HunterWilkins <wilkins.hunter@gmail.com>
2019-11-22 12:56:16 -0600

    Very basic setup, testing for heroku deployment. If this doesn't work, then dedicate all your effort into figuring out how heroku deployment should work.

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/memes"
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.use(session({
+    secret: "youtubecommentsaren'tthatbad",
+    resave: false,
+    saveUninitialized: true,
+    store: new MongoStore({mongooseConnection: mongoose.connection}),
+}));
+
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true , useUnifiedTopology: true });
+
+// Routes
+require("./routes/api.js")(app);
+require("./routes/html.js")(app);
+
+app.listen(PORT, () => {
+    console.log("Listening in on " + PORT);
+});
