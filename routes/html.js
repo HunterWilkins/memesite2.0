8b6e92a922678309fb1320dac78d6ea99dac2ca0
HunterWilkins <wilkins.hunter@gmail.com>
2019-11-22 16:57:45 -0600

    Added clarifier to console.log(userId)

--- routes/html.js
+++ routes/html.js
@@ -1,6 +1,6 @@
 module.exports = function(app) {
     app.get("/", function(req, res) {
-        console.log(req.session.userId);
+        console.log("User Id: " + req.session.userId);
         res.render("frontpage", {userId: req.session.userId});
     });
 }
