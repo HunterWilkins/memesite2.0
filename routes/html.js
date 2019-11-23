a1d915c3aaa274cab0fdcdd0743c6cc5983541bf
HunterWilkins <wilkins.hunter@gmail.com>
2019-11-22 18:21:34 -0600

    Added console.logs for if (userId) in api.js

--- routes/html.js
+++ routes/html.js
@@ -2,10 +2,12 @@ module.exports = function(app) {
     app.get("/", function(req, res) {
         console.log("User Id: " + req.session.userId);
         if (req.session.userId) {
+            console.log("Found the user.");
             res.render("frontpage", {userId: req.session.userId});
         }
 
         else {
+            console.log("Not Logged In");
             res.render("frontpage");
         }
     });
