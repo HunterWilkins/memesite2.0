60a74522f18fb6cd0469f6e4efd8762757b8562b
HunterWilkins <wilkins.hunter@gmail.com>
2019-11-27 14:24:09 -0600

    Got rid of extra api call that was unnecessary with the new html route code

--- public/js/frontpage.js
+++ public/js/frontpage.js
@@ -30,27 +30,8 @@ $(document).ready(function() {
 
     $("#genre-picker").on("change", function() {
         console.log($(this).val());
-        $("#posts").empty();
         window.location.replace(`/frontpage/${$(this).val()}`);
-        $.getJSON(`/api/posts/${$(this).val()}`, function(data) {
-            if (data.length !== 0) {
-                data.forEach(item => {
-                    $("#posts").prepend(
-                        `<a class = "post" href = "/posts/${item.id}">
-                            <p class = "post-title">${item.title}</p>
-                            <p class = "post-genre">${item.genre}</p>
-                            <p class = "post-author"><em>${item.author}</em></p>
-                        </a>
-                        `
-                    );  
-                })    
-            }
-
-            else {
-                $("#posts").text("No results.");
-            }
-        })
-    })
+    });
 
     
 });
