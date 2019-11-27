1dc5098305e2d3c84b77c1744bb4ee058405a409
HunterWilkins <wilkins.hunter@gmail.com>
2019-11-27 14:00:10 -0600

    Incorporated No Results feedback on page load.

--- public/js/frontpage.js
+++ public/js/frontpage.js
@@ -1,21 +1,31 @@
 $(document).ready(function() {
     let postQuantity = 0;
     let pageGenre = window.location.pathname.split("/")[2]
-    
-    $("#genre-picker").val(window.location.pathname.split("/")[2]);
+    if (pageGenre) {
+        $("#genre-picker").val(window.location.pathname.split("/")[2]);
+    }
+    else {
+        pageGenre = "all";
+    }
     
     $.getJSON(`/api/posts/${pageGenre}`, function(data) {
-        data.forEach(item => {
+        if (data.length !== 0) {
             postQuantity++;
-            $("#posts").prepend(
-                `<a class = "post" href = "/posts/${item.id}">
-                    <p class = "post-title">${item.title}</p>
-                    <p class = "post-genre">${item.genre}</p>
-                    <p class = "post-author"><em>${item.author}</em></p>
-                </a>
-                `
-            );
-        });
+
+            data.forEach(item => {
+                $("#posts").prepend(
+                    `<a class = "post" href = "/posts/${item.id}">
+                        <p class = "post-title">${item.title}</p>
+                        <p class = "post-genre">${item.genre}</p>
+                        <p class = "post-author"><em>${item.author}</em></p>
+                    </a>
+                    `
+                );  
+            })    
+        }
+        else {
+            $("#posts").text("No results.");
+        }
     });
 
     $("#genre-picker").on("change", function() {
