f4b9a03edcb04f7a9bc1d577f881a999a2dff9d4
HunterWilkins <wilkins.hunter@gmail.com>
2019-12-11 13:56:26 -0600

    Fixed error showing ALL posts in settings menu. Just had to switch the variable names.

--- routes/api.js
+++ routes/api.js
@@ -98,7 +98,7 @@ module.exports = function(app) {
                 });
                 
                 let response = {
-                    posts: dbPosts,
+                    posts: myPosts,
                     comments: myComments
                 }
 
